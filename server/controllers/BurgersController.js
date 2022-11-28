49488545613c32ecf8720ea4661b3d65d3d2c36a
jon-cron <jonathanacm95@gmail.com>
2022-11-28 16:21:05 -0700

    idk

--- server/controllers/BurgersController.js
+++ server/controllers/BurgersController.js
@@ -1,15 +1,26 @@
-import { response } from "express";
+// import { response } from "express";
 import { burgersService } from "../services/BurgersService.js";
 import BaseController from "../utils/BaseController.js";
 
 export class BurgersController extends BaseController {
   constructor() {
-    super("api/burgers").get("", this.getAll);
+    super("api/burgers");
+      this.router
+    .get('', this.getAll)
+    .get("/:id", this.getOne);
   }
-  async getAll(req, res, next) {
+  async getAll(request, response, next) {
     try {
       const burgers = await burgersService.getAll();
       return response.send(burgers);
+    } catch (error) {
+      next(error);
+    }
+  }
+  async getOne(request, response, next) {
+    try {
+      const burger = await burgersService.getOne(request.params.id);
+      return response.send({ burger, message: "here is a burger" });
     } catch (error) {
       next(error);
     }
--- server/services/BurgersService.js
+++ server/services/BurgersService.js
@@ -1,9 +1,15 @@
 import { dbContext } from "../db/DbContext.js";
+import { BadRequest } from "../utils/Errors.js";
 
 class BurgersService {
   async getAll() {
     const burgers = await dbContext.Burgers;
     return burgers;
+  }
+  async getOne(burgerId) {
+    const burger = await dbContext.Burgers.find((b) => b.id == burgerId);
+    if (!burger) throw new BadRequest("no burger named" + burgerId);
+    return burger;
   }
 }
 
